2825dfa8a9099536cc43db58e9c5e037bef58668
mariammamniashvili <65232086+mariammamniashvili@users.noreply.github.com>
2020-09-23 10:03:09 +0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('testing','AuthentificationController@index');
+Route::get('tpl','AuthentificationController@index');
 Route::get('platform','UserController@platform');
 Route::get('/', 'TestController@gela');
 Route::get('test', 'UserController@CarCategory');
